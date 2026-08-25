4512d7b9908336e0fc48bec4ae32c2a7539c8524
paulantipovcard1-dev <paulantipov.card1@gmail.com>
2026-08-25 11:32:04 +0200

    Add files via upload

---
+++ LitRadioApp/app/src/main/java/link/litradio/app/PlaybackService.java
@@ -0,0 +1,41 @@
+package link.litradio.app;
+
+import androidx.annotation.Nullable;
+import androidx.media3.common.MediaItem;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.session.MediaSession;
+import androidx.media3.session.MediaSessionService;
+
+public class PlaybackService extends MediaSessionService {
+
+    private static final String STREAM_URL = "https://litradio.link/radio.mp3";
+
+    private MediaSession mediaSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ExoPlayer player = new ExoPlayer.Builder(this).build();
+        player.setMediaItem(MediaItem.fromUri(STREAM_URL));
+        player.prepare();
+
+        mediaSession = new MediaSession.Builder(this, player).build();
+    }
+
+    @Nullable
+    @Override
+    public MediaSession onGetSession(MediaSession.ControllerInfo controllerInfo) {
+        return mediaSession;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mediaSession != null) {
+            mediaSession.getPlayer().release();
+            mediaSession.release();
+            mediaSession = null;
+        }
+        super.onDestroy();
+    }
+}
